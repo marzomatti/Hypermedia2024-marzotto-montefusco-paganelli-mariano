81e9027cffbec4504e90c548a1e6aeed76d2cd10
Mattia Marzotto <mattia.marzotto@mail.polimi.it>
2024-05-21 13:06:19 +0200

    introduced about us page

--- pages/about.vue
+++ pages/about.vue
@@ -1,5 +1,49 @@
+
+
 <template>
-    <div>
-        About us
+    <div class="container">
+        <h1 style="font-size: 24px; text-decoration: underline;">About us</h1>
+        <img src="../assets/images/group_pic.jpeg" alt="Group Image">
+    </div>
+    <div class="description">
+        <h2 style="font-size: 24px; text-decoration: underline;">Our Mission</h2>
+        <p>
+        At the Women's Violence Center for Abuse, our mission is to provide a safe haven for women who have 
+        experienced violence and abuse, offering comprehensive support services to empower them towards a 
+        life of dignity, security, and independence. We are committed to breaking the cycle of violence 
+        through compassionate care, advocacy, and education.
+        </p>
+        <p>
+        At the Women's Violence Center for Abuse, we are steadfast in our commitment to providing a lifeline to those 
+        in need. Every woman deserves to live a life free from violence, and we are here to support them every step of 
+        the way. Together, we can build a safer, more just world for all women.
+        </p>
     </div>
 </template>
+
+
+
+<style>
+.container {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    text-align: center;
+    margin-top: 20px;
+}
+
+.description {
+    margin-top: 20px;
+    font-size: 18px;
+    line-height: 1.5;
+    width: 50%;
+    margin-left: 25%;
+}
+
+img {
+    width: 300px;
+    height: auto;
+    margin-top: 20px;
+}
+</style>
