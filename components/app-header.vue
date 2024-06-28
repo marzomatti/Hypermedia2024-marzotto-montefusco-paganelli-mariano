a9c27b70bd8c8a800c58902db10303aa2d62fe34
Christian <christian.mariano@mail.polimi.it>
2024-06-28 16:25:42 +0200

    fixed header

--- components/app-header.vue
+++ components/app-header.vue
@@ -16,7 +16,7 @@
                 <span>{{ item.name }}</span>
               </NuxtLink>
               <DropdownMenu v-else :key="item.id" :categories="activityCategories" @click="setCurrentPage(item.name)">
-                <NuxtLink class="text-black hover:bg-red-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium flex items-center">
+                <NuxtLink to="/Activities" class="text-black hover:bg-red-400 hover:text-white rounded-md px-3 py-2 text-sm font-medium flex items-center">
                   <span>{{ item.name }}</span>
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4 ml-1">
                     <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
