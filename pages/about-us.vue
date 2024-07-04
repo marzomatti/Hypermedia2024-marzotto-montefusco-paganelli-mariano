904d061eee41bc6d6c9b4c05f1a4b49cd4f952ed
valeria-mf <valeria.montefusco@mail.polimi.it>
2024-07-04 14:55:04 +0200

    about us minor updates

--- pages/about-us.vue
+++ pages/about-us.vue
@@ -79,11 +79,11 @@
 
     <!-- Sezione "Our Expert Team" -->
     <div class="bg-gray-200 w-full px-4 lg:px-16 py-8">
+    <h1 class="text-3xl py-8 md:py-12 md:text-5xl font-bold text-center">Meet Our Expert Team</h1>
 <div class="flex flex-wrap justify-center gap-20 items-center py-12">
         <img class="rounded-3xl w-full max-w-lg object-cover" src="/assets/img/about_2.jpg" alt="Team Image 1" />
         <img class="rounded-3xl w-full max-w-lg object-cover" src="/assets/img/about_3.jpg" alt="Team Image 2" />
       </div>
-      <h1 class="text-3xl py-8 md:py-12 md:text-5xl font-bold text-center">Meet Our Expert Team</h1>
       <div class="flex flex-col text-xl text-center pb-10 md:pb-12 md:flex-row justify-center px-4 lg:px-16">
         At No Woman Alone, our team consists of highly skilled and compassionate professionals dedicated to supporting women affected by violence. Our experts bring a wealth of experience in various fields, including psychology, social work, and advocacy. We work tirelessly to provide comprehensive care and guidance, helping women to heal and thrive. Our team is here to listen, support, and empower you every step of the way. Learn more about the individuals who make our mission possible and their unwavering commitment to creating a safer world for all women.
       </div>
