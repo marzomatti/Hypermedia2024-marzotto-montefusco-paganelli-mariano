1a2bf1fd1737a61488e6242d5ee77b1a9da81a06
valeria-mf <valeria.montefusco@mail.polimi.it>
2024-07-05 16:35:32 +0200

    color change

--- components/dropdown-menu.vue
+++ components/dropdown-menu.vue
@@ -4,10 +4,10 @@
     <div v-if="showDropdown" class="absolute left-0 mt-2 w-max bg-white shadow-lg rounded-lg z-50">
       <div class="p-4 flex space-x-6">
         <div v-for="category in categories" :key="category.name" class="w-1/2">
-          <NuxtLink :to="category.href" class="block px-4 py-2 hover:bg-secondary-color hover:text-white rounded-md text-sm font-bold mb-2">{{ category.name }}</NuxtLink>
+          <NuxtLink :to="category.href" class="block px-4 py-2 hover:bg-secondary-color hover:text-white text-blue rounded-md text-sm font-bold mb-2">{{ category.name }}</NuxtLink>
           <ul class="space-y-2">
             <li v-for="service in services" :key="service.id">
-              <NuxtLink :to="service.href" class="block px-4 py-2 hover:bg-secondary-color hover:text-white rounded-md">
+              <NuxtLink :to="service.href" class="block px-4 py-2 text-blue hover:bg-secondary-color hover:text-white rounded-md">
                 {{ service.name }}
               </NuxtLink>
             </li>
--- pages/Team/index.vue
+++ pages/Team/index.vue
@@ -1,21 +1,21 @@
 <template>
   <section class="bg-white py-12 px-12">
-    <h1 class="font-sans font-extrabold no-italic text-[40px] md:text-[56px] md:leading-[66px] text-center">Our Team</h1>
+    <h1 class="font-sans font-extrabold no-italic text-[40px] md:text-[56px] md:leading-[66px] text-center text-blue">Our Team</h1>
 
     <div class="container mx-auto">
       <div class="grid grid-cols-1 md:grid-cols-2 gap-8 text-center">
         <!-- Staff Block -->
         <div class="bg-white p-6 rounded-3xl shadow-lg hover:shadow-xl transition duration-300">
           <img src="/assets/img/team/people_staff.jpg" alt="Staff" class="w-full h-auto object-cover mb-4 rounded-3xl aspect-portrait"/>
-          <h3 class="text-2xl font-semibold text-gray-800 mb-2">Staff</h3>
-          <p class="text-gray-600 mb-4">Meet our dedicated staff members who work tirelessly to provide support and services to women in need.</p>
+          <h3 class="text-2xl font-semibold text-blue mb-2">Staff</h3>
+          <p class="text-blue mb-4">Meet our dedicated staff members who work tirelessly to provide support and services to women in need.</p>
           <Button text="Discover more ->" link="/team/staff" class="mt-4"/>
         </div>
         <!-- Volunteers Block -->
         <div class="bg-white p-6 rounded-3xl shadow-lg hover:shadow-xl transition duration-300">
           <img src="/assets/img/team/people_volounteers.jpg" alt="Volunteers" class="w-full h-auto object-cover mb-4 rounded-3xl aspect-portrait"/>
-          <h3 class="text-2xl font-semibold text-gray-800 mb-2">Volunteers</h3>
-          <p class="text-gray-600 mb-4">Discover the passionate volunteers who help us make a difference in the lives of women facing violence.</p>
+          <h3 class="text-2xl font-semibold text-blue mb-2">Volunteers</h3>
+          <p class="text-blue mb-4">Discover the passionate volunteers who help us make a difference in the lives of women facing violence.</p>
           <Button text="Discover more ->" link="/team/volunteers" class="mt-4"/>
         </div>
       </div>
