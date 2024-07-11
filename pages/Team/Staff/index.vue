f90525380f1ce34cc351ad30135623b3c9279f60
valeria-mf <valeria.montefusco@mail.polimi.it>
2024-07-11 18:54:24 +0200

    add animations in about us, not good ones

--- pages/Team/Staff/index.vue
+++ pages/Team/Staff/index.vue
@@ -1,20 +1,13 @@
 <template>
   <div>
-    <Breadcrumb />
+    <Breadcrumb/>
     <section class="py-12 px-4 lg:px-24">
       <h1 class="text-4xl text-blue font-bold text-center mb-8">Our Staff</h1>
       <p class="text-lg text-blue text-center mb-12">
         Meet our dedicated staff committed to supporting and advocating for women affected by violence. Our team is composed of experienced professionals who are passionate about making a difference. Each member brings a unique set of skills and expertise to provide comprehensive support and create a safe and empowering environment for all. We are here to help, listen, and guide you every step of the way.
       </p>
       <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-8">
-        <nuxt-link
-          v-for="(person, index) in staff"
-          :key="person.id"
-          :to="`/team/staff/${person.id}`"
-          v-show="isVisible(index)"
-          class="bg-white p-6 rounded-lg shadow-lg text-center transform transition-transform duration-700 hover:scale-105 animate-fadeIn"
-          
-        >
+        <nuxt-link v-for="person in staff" :key="person.id" :to="`/team/staff/${person.id}`" class="bg-white p-6 rounded-lg shadow-lg text-center transform transition-transform duration-300 hover:scale-105">
           <img :src="'/team/staff' + person.image" alt="Staff Photo" class="w-full h-auto object-cover rounded-lg mb-4 aspect-portrait">
           <h3 class="text-xl font-semibold text-blue">{{ person.name }} {{ person.surname }}</h3>
           <p class="text-blue">{{ person.role }}</p>
@@ -25,42 +18,14 @@
 </template>
 
 <script setup>
-import { ref, onMounted } from 'vue';
-
 useHead({
   title: 'Staff',
 })
 const { data: staff, error, loading } = await useFetch('/api/staff');
 
-const visibleIndices = ref([]);
-
-const isVisible = (index) => visibleIndices.value.includes(index);
-
-onMounted(() => {
-  staff.value.forEach((_, index) => {
-    setTimeout(() => {
-      visibleIndices.value.push(index);
-    }, index * 500); // Delay each card by 500ms
-  });
-});
 </script>
 
 <style scoped>
-@keyframes fadeIn {
-  0% {
-    opacity: 0;
-    transform: translateY(20px);
-  }
-  100% {
-    opacity: 1;
-    transform: translateY(0);
-  }
-}
-
-.animate-fadeIn {
-  animation: fadeIn 0.8s ease-out forwards;
-}
-
 .rounded-lg {
   border-radius: 1.5rem;
 }
@@ -70,8 +35,7 @@ onMounted(() => {
 }
 
 .shadow-lg {
-  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1),
-    0 4px 6px -2px rgba(0, 0, 0, 0.05);
+  box-shadow: 0 10px 15px -3px rgba(0, 0, 0, 0.1), 0 4px 6px -2px rgba(0, 0, 0, 0.05);
 }
 
 .text-gray-800 {
