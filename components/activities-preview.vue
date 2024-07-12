2f1feb709ced23831b3d4d8447f3f1c7dc79635f
valeria-mf <valeria.montefusco@mail.polimi.it>
2024-07-12 21:45:53 +0200

    improve consistency all over the pages

--- components/activities-preview.vue
+++ components/activities-preview.vue
@@ -1,14 +1,17 @@
 <template>
-  <section class="bg-primary-color py-12 px-12 hiddenItem">
+  <section class="bg-primary-color py-6 px-12 hiddenItem">
     <div class="container mx-auto text-center">
-      <h2 class="text-4xl font-bold text-white mb-8">Our Activities</h2>
+      <TitleColor
+      title="Our Activities"
+      :description = "text"
+    />
       <div class="grid grid-cols-1 md:grid-cols-2 gap-8 mb-8">
         <!-- Services Block -->
         <NuxtLink to="/Activities/Services" class="rounded-3xl flex items-center hiddenItem">
           <div class="bg-white p-6 rounded-3xl shadow-lg transition duration-500 hover:shadow-xl hover:scale-105">
             <img data-src="/services_preview.jpg" alt="Services Image" class="lazy w-full h-64 object-cover mb-4 rounded-3xl"/>
-            <h3 class="text-xl font-semibold text-blue mb-2">Services</h3>
-            <p class="text-blue">
+            <h3 class="text-4xl font-bold font-sans no-italic text-blue mb-2">Services</h3>
+            <p class="text-blue font-sans no-italic">
               Offering counseling, legal help, and recovery programs to support and empower women.
             </p>
           </div>
@@ -18,14 +21,14 @@
         <NuxtLink to="/Activities/Projects" class="rounded-3xl flex items-center hiddenItem">
           <div class="bg-white p-6 rounded-3xl shadow-lg hover:shadow-xl transition duration-500 hover:scale-105">
             <img data-src="/projects_preview.jpg" alt="Projects Image" class="lazy w-full h-64 object-cover mb-4 rounded-3xl"/>
-            <h3 class="text-xl font-semibold text-blue mb-2">Projects</h3>
-            <p class="text-blue">
+            <h3 class="text-4xl font-bold font-sans no-italic text-blue mb-2">Projects</h3>
+            <p class="text-blue font-sans no-italic">
               Raising awareness and actively advocating for women's rights through community projects.
             </p>
           </div>
         </NuxtLink>
       </div>
-      <NuxtLink to="/activities" class="inline-block border-2 border-white text-white px-6 py-3 rounded-full font-semibold hover:bg-white hover:text-primary-color transition duration-300">
+      <NuxtLink to="/activities" class="font-sans no-italic inline-block border-2 border-white text-white px-6 py-3 rounded-full font-semibold hover:bg-white hover:text-primary-color transition duration-300">
         See all activities &rarr;
       </NuxtLink>
     </div>
