4ae39c500a0e4874d30f1e285a33fe2474363b94
andreeapaganelli <160610286+andreeapaganelli@users.noreply.github.com>
2024-07-13 18:32:34 +0200

    fixed errors handling in chatbot

--- pages/virtual-assistant.vue
+++ pages/virtual-assistant.vue
@@ -1,11 +1,11 @@
 <template>
   <main>
     <!-- Title Section -->
-     <TitleColor
+    <TitleColor
       title="Are you a victim of domestic violence?"
       description="Chat now with our virtual assistant!"
     />
-    
+
     <!-- Main Content Section -->
     <section class="bg-white py-12 px-12">
       <div class="container mx-auto flex flex-col md:flex-row items-center">
@@ -13,28 +13,34 @@
         <div class="w-full md:w-1/3 pr-8 mb-8 md:mb-0 text-center md:text-left">
           <h2 class="text-2xl font-bold text-blue mb-4">How We Can Help</h2>
           <p class="text-blue text-lg mb-4 font-bold">
-            Our virtual assistant is here to support you with compassion and understanding.
+            Our virtual assistant is here to support you with compassion and
+            understanding.
           </p>
           <p class="text-gray-700 text-lg mb-4">
-            If you are facing domestic violence, you don't have to go through it alone. Our assistant provides a safe space to ask questions and get guidance on your rights and options.
+            If you are facing domestic violence, you don't have to go through it
+            alone. Our assistant provides a safe space to ask questions and get
+            guidance on your rights and options.
           </p>
           <p class="text-blue text-lg mb-4">
-            Whether you need to know more about your legal rights, how to start legal procedures, or where to find help, we're here for you. Just enter your question below.
+            Whether you need to know more about your legal rights, how to start
+            legal procedures, or where to find help, we're here for you. Just
+            enter your question below.
           </p>
           <p class="text-blue text-lg mb-4">
-            Remember, in an emergency, always contact the authorities immediately. We are here to offer support and guidance in a non-judgmental and caring manner.
+            Remember, in an emergency, always contact the authorities
+            immediately. We are here to offer support and guidance in a
+            non-judgmental and caring manner.
           </p>
         </div>
-        <!-- Chat section -->
         <div
           class="w-full md:w-2/3 flex flex-col bg-white rounded-3xl shadow-md p-8 resize-y overflow-auto"
-          style="min-height: 600px; max-height: 1000px;"
+          style="min-height: 600px; max-height: 600px"
         >
           <!-- Chat messages -->
           <div
             ref="chatMessages"
-            class="flex-1 bg-gray-100 rounded-3xl p-4 overflow-y-auto"
-            style="max-height: calc(100% - 60px);"
+            class="flex-1 bg-gray-100 rounded-3xl p-4 overflow-y-scroll"
+            style="max-height: calc(100% - 60px)"
           >
             <div v-for="message in messages" :key="message.id" class="mb-4">
               <div
@@ -105,7 +111,14 @@ const sendMessage = async () => {
     await nextTick();
     scrollToBottom();
 
-    const answer = await getAnswerFromOpenAI(question);
+    const answer = await getAnswerFromOpenAI(question).catch((error) => {
+      console.error("Error getting answer from OpenAI:", error);
+      messages.value.push({
+        id: Date.now() + 1,
+        type: "answer",
+        content: "I'm sorry, There was an error processing your request. Please try again later.",
+      });
+    });
     const htmlAnswer = convertResponseToHTML(answer).trim();
     messages.value.push({
       id: Date.now() + 1,
