4e1b91d848a27b2900a988cfcf0631dbefbc2a97
andreeapaganelli <160610286+andreeapaganelli@users.noreply.github.com>
2024-07-13 10:47:57 +0200

    minor fix in projects-preview

--- components/projects-preview.vue
+++ components/projects-preview.vue
@@ -2,23 +2,37 @@
   <section class="bg-gray-100 px-12">
     <div class="container mx-auto">
       <div class="p-6 rounded-lg">
-        <div class="items-center mb-6 mt-4 ">
-          <h2 class="hiddenItem text-4xl font-extrabold text-blue text-center">Most Relevant Projects</h2>
+        <div class="items-center mb-6 mt-4">
+          <h2 class="hiddenItem text-4xl font-extrabold text-blue text-center">
+            Most Relevant Projects
+          </h2>
         </div>
-        <div class="hiddenItem grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div
+          class="hiddenItem grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8"
+        >
           <nuxt-link
             v-for="project in mostRelevantProjects"
             :key="project.id"
             :to="`/activities/projects/${project.id}`"
             class="bg-white p-6 rounded-3xl shadow-lg transition duration-500 hover:shadow-xl hover:scale-105"
           >
-            <img :src="'/projects' + project.image" alt="project.name" class="w-full h-60 object-cover mb-2 rounded-3xl mb-4" />
-            <h4 class="text-xl font-semibold text-blue mb-2">{{ project.name }}</h4>
+            <img
+              :src="'/projects' + project.image"
+              alt="project.name"
+              class="w-full h-60 object-cover rounded-3xl mb-2"
+            />
+            <h4 class="text-xl font-semibold text-blue mb-2">
+              {{ project.name }}
+            </h4>
             <p class="text-blue mb-2">{{ project.description_s }}</p>
           </nuxt-link>
         </div>
         <div class="hiddenItem flex justify-center mt-6">
-          <Button :text="'See all projects &rarr;'" :link="'/activities/projects'" class="mt-4"/>
+          <Button
+            :text="'See all projects &rarr;'"
+            :link="'/activities/projects'"
+            class="mt-4"
+          />
         </div>
       </div>
     </div>
@@ -26,8 +40,8 @@
 </template>
 
 <script setup>
-import { onMounted, computed } from 'vue';
-const { data: projects, error, loading } = await useFetch('/api/projects');
+import { onMounted, computed } from "vue";
+const { data: projects, error, loading } = await useFetch("/api/projects");
 const router = useRouter();
 
 const navigateTo = (id) => {
@@ -41,19 +55,22 @@ const mostRelevantProjects = computed(() => {
 });
 
 onMounted(() => {
-  const observer = new IntersectionObserver(entries => {
-    entries.forEach(entry => {
-      if (entry.isIntersecting) {
-        entry.target.classList.add('show');
-        entry.target.classList.remove('hiddenItem');
-      }
-    });
-  }, {
-    rootMargin: '-20% 0px -10% 0px'
-  });
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("show");
+          entry.target.classList.remove("hiddenItem");
+        }
+      });
+    },
+    {
+      rootMargin: "-20% 0px -10% 0px",
+    }
+  );
 
-  const hiddenElements = document.querySelectorAll('.hiddenItem');
-  hiddenElements.forEach(element => {
+  const hiddenElements = document.querySelectorAll(".hiddenItem");
+  hiddenElements.forEach((element) => {
     observer.observe(element);
   });
 });
@@ -62,7 +79,8 @@ onMounted(() => {
 <style scoped>
 .hiddenItem {
   opacity: 0;
-  transition: opacity 0.5s ease-in-out, filter 0.5s ease-in-out, transform 0.5s ease-in-out;
+  transition: opacity 0.5s ease-in-out, filter 0.5s ease-in-out,
+    transform 0.5s ease-in-out;
   filter: blur(10px);
   transform: translateY(30px);
 }
